7b4390c4a96ce00967e9a676efdd52e48896d898
clarklin <clarkms101@gmail.com>
2020-07-02 21:58:12 +0800

    取得注入的UserConfig,顯示在頁面上

--- WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -5,21 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using WebApplication1.Config;
 
 namespace WebApplication1.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly UserConfig _userConfig;
+        public string UserName { get; set; }
+        public string UserNo { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IOptions<UserConfig> userConfig)
         {
             _logger = logger;
+            _userConfig = userConfig.Value;
         }
 
         public void OnGet()
         {
-
+            UserName = _userConfig.UserName;
+            UserNo = _userConfig.UserNo;
         }
     }
 }
